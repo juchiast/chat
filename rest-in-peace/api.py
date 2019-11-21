685a8c41f0ccba707c517c8f026dbbd6ce1b5153
Trung-Hieu Ho <trunghieu.ho@ved.com.vn>
2019-11-21 16:58:46 +0700

    move main logics to routine

--- rest-in-peace/api.py
+++ rest-in-peace/api.py
@@ -1,55 +1,32 @@
-import cassandra.cluster
 from flask import Flask, request
 
 from exception import catch_panic
-from model import MessageIdFactory
-from routine import get_worker_id
+from routine import load_messages, send_message
 
-
-worker_id = get_worker_id()
-msg_factory = MessageIdFactory(worker_id)
 app = Flask(__name__)
-cluster = cassandra.cluster.Cluster()
 
 
 @app.route("/ping", methods=["GET"])
 @catch_panic()
-def ping():
+def ping_api():
     return {"message": "pong"}
 
 
 @app.route("/<int:room_id>/", methods=["GET"])
 @catch_panic()
-def load_messages(room_id):
+def load_messages_api(room_id):
     limit = int(request.args.get('limit'))
     before = request.args.get('before')
-    session = cluster.connect('chat')
-    if not before:
-        rows = session.execute("select user_name, id, content from messages where room_id=%s order by id desc limit %s" % (room_id, limit))
-    else:
-        before = int(before)
-        rows = session.execute("select user_name, id, content from messages where room_id=%s and id<%s order by id desc limit %s" % (room_id, before, limit))
-    res = []
-    for row in rows:
-        row_id = row[1]
-        res.append({
-            "id": str(row_id),
-            "user_name": row[0],
-            "content": row[2],
-            "timestamp": msg_factory.get_timestamp_from_id(row_id)
-        })
-    return {"messages": res}
+    return {"messages": load_messages(room_id, limit, before)}
 
 
 @app.route("/<int:room_id>/", methods=["POST"])
 @catch_panic()
-def send_message(room_id):
+def send_message_api(room_id):
     body = request.json
     user_name = str(body["user_name"])
     message = str(body["message"])
-    session = cluster.connect('chat')
-    msg_id = msg_factory.generate_message_id()
-    session.execute("insert into messages (id, content, user_name, room_id) values (%s, %s, %s, %s)", (msg_id, message, user_name, room_id))
+    send_message(room_id, user_name, message)
     return {}
 
 
--- rest-in-peace/routine.py
+++ rest-in-peace/routine.py
@@ -1,6 +1,45 @@
+import cassandra.cluster
+
+import setting
+from model import MessageIdFactory
+
 
 def get_worker_id():
     # TODO:
     # Implement ID counter in Redis
     # https://blog.twitter.com/engineering/en_us/a/2010/announcing-snowflake.html
     return 1
+
+
+cluster = cassandra.cluster.Cluster()
+worker_id = get_worker_id()
+msg_factory = MessageIdFactory(worker_id)
+
+
+def load_messages(room_id, limit, before=None):
+    session = cluster.connect(setting.APP_NAME)
+    if not before:
+        rows = session.execute(
+            "select user_name, id, content from messages where room_id=%s order by id desc limit %s" % (room_id, limit))
+    else:
+        before = int(before)
+        rows = session.execute(
+            "select user_name, id, content from messages where room_id=%s and id<%s order by id desc limit %s" % (
+            room_id, before, limit))
+    messages = []
+    for row in rows:
+        row_id = row[1]
+        messages.append({
+            "id": str(row_id),
+            "user_name": row[0],
+            "content": row[2],
+            "timestamp": msg_factory.get_timestamp_from_id(row_id)
+        })
+    return messages
+
+
+def send_message(room_id, user_name, message):
+    session = cluster.connect(setting.APP_NAME)
+    msg_id = msg_factory.generate_message_id()
+    session.execute("insert into messages (id, content, user_name, room_id) values (%s, %s, %s, %s)",
+                    (msg_id, message, user_name, room_id))
