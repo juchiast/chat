6d81b66447834f95500a0daf292c93cca5bd6a4f
Trung-Hieu Ho <trunghieu.ho@ved.com.vn>
2019-11-21 15:18:29 +0700

    move exception handler to new file

---
+++ rest-in-peace/exception.py
@@ -0,0 +1,26 @@
+import traceback
+from functools import wraps
+from flask import jsonify
+
+
+def error_response(msg):
+    return jsonify({"error_code": 1, "error_message": msg}), 400
+
+
+def success_response(key=None, value=None):
+    if not key:
+        return jsonify({"error_code": 0}), 200
+    return jsonify({"error_code": 0, key: value}), 200
+
+
+def catch_panic():
+    def _decorator(func):
+        @wraps(func)
+        def __decorator(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                traceback.print_exc()
+                return error_response("%s: %s" % (type(e).__name__, str(e)))
+        return __decorator
+    return _decorator
