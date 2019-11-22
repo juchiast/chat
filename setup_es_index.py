3c6fabe93979bebd4f0c81346036f4f49e2c48c3
Duy Do <juchiast@gmail.com>
2019-11-22 14:11:32 +0700

    setup_es_index.py

---
+++ setup_es_index.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import elasticsearch
+
+INDEX_NAME = 'chat'
+INDEX_TEMPLATE = {
+    'mappings': {
+        '_source': {
+            'includes': [
+                'id',
+                'room_id',
+            ]
+        },
+        'properties': {
+            'id': {
+                'type': 'long'
+            },
+            'room_id': {
+                'type': 'long'
+            },
+            'user_name': {
+                'type': 'text'
+            },
+            'content': {
+                'type': 'text',
+                'fields': {
+                    'lang_analyzed': {
+                        'type': 'text',
+                        'analyzer': 'english'
+                    }
+                }
+            }
+        }
+    }
+}
+
+def main():
+    es = elasticsearch.Elasticsearch()
+    indexes = elasticsearch.client.IndicesClient(es)
+
+    if indexes.exists(INDEX_NAME):
+        return
+    print('Creating index')
+    indexes.create(index=INDEX_NAME, body=INDEX_TEMPLATE)
+
+main()
