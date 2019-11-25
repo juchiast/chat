9c0518830164e0f2ac00cb3befe3935aa86b735b
Duy Do <juchiast@gmail.com>
2019-11-25 09:03:44 +0700

    example messages

--- chat-client/src/chat-window.js
+++ chat-client/src/chat-window.js
@@ -4,6 +4,8 @@ import Input from './input';
 
 import './App.css';
 
+
+
 export default class ChatWindow extends React.Component {
     constructor(props) {
         super(props);
@@ -17,7 +19,11 @@ export default class ChatWindow extends React.Component {
     // }
 
     getMessages() {
-        return []
+        let TEST_MESSAGES = [
+            { id: "1", timestamp: 0, user_name: "duy", content: "Hello" },
+            { id: "2", timestamp: 0, user_name: "duy", content: "Hello" }
+        ];
+        return TEST_MESSAGES;
     }
 
     // when has new messages, do sth
@@ -32,8 +38,8 @@ export default class ChatWindow extends React.Component {
         return (
             <div className={this.props.openSearchBox ? 'app_content' : 'app_content_0'}>
                 <div className="chat_window">
-                    <Messages user={this.props.user} messages={this.state.messages}/>
-                    <Input sendMessage={this.sendnewMessage.bind(this)}/>
+                    <Messages user={this.props.user} messages={this.state.messages} />
+                    <Input sendMessage={this.sendnewMessage.bind(this)} />
                 </div>
             </div>
         )
--- chat-client/src/message-list.js
+++ chat-client/src/message-list.js
@@ -3,11 +3,11 @@ import Message from './message-item';
 
 
 export default class MessageItem extends React.Component {
-    render () {
+    render() {
         return (
             <ul className="messages">
                 {this.props.messages.map(item =>
-                    <Message key={item.id} user={item.userId === this.props.user? true: false} message={item.message}/>
+                    <Message key={item.id} user={item.user_name === this.props.user ? true : false} message={item.content} />
                 )}
             </ul>
         )
