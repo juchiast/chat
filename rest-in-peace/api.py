4ed5d97ed97c439bc57710de77e4b2cd78ae4fd6
Do Duy <juchiast@gmail.com>
2019-11-25 01:57:30 -0800

    Client (#5)

--- rest-in-peace/api.py
+++ rest-in-peace/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 from wrap_response import wrap_response
 from routine import load_messages, send_message, search
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/ping", methods=["GET"])
